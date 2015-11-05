7906d7ccda043f48132fb43cca88bd2580b4e436
unknown <l075695@AU21CALP3340.infau.wbcau.westpac.com.au>
2015-11-05 14:16:03 +1100

    html inspector commented.

--- grunt/tasks-list.js
+++ grunt/tasks-list.js
@@ -1,34 +1,34 @@
 module.exports = function tasks(grunt) {
-  //////////// tasks
-  // grunt-w3c-validation
-  // a11y
-  /* grunt.registerTask('html', ['validation', 'accessibility']); //'grunt-html',
+	//////////// tasks
+	// grunt-w3c-validation
+	// a11y
+	/* grunt.registerTask('html', ['validation', 'accessibility']); //'grunt-html',
 
-   // grunt-icon
-   grunt.registerTask('assets', []);
+	 // grunt-icon
+	 grunt.registerTask('assets', []);
 
-   // javascript
-   grunt.registerTask('scripts', ['jshint', 'uglify']); //'lintjs', 'uglify'
+	 // javascript
+	 grunt.registerTask('scripts', ['jshint', 'uglify']); //'lintjs', 'uglify'
 
-   // lint less
-   grunt.registerTask('styles', ['less', 'autoprefixer']); //'less','auto-prefixer'
+	 // lint less
+	 grunt.registerTask('styles', ['less', 'autoprefixer']); //'less','auto-prefixer'
 
-   // build
-   grunt.registerTask('build', ['html', 'assets', 'scripts', 'styles']);
+	 // build
+	 grunt.registerTask('build', ['html', 'assets', 'scripts', 'styles']);
 
-   // Default task
-   grunt.registerTask('default', ['build', 'connect', 'watch']);*/
+	 // Default task
+	 grunt.registerTask('default', ['build', 'connect', 'watch']);*/
 
-  //Setup
-  grunt.registerTask('setup', ['copy']);
+	//Setup
+	grunt.registerTask('setup', ['copy']);
 
-  //Develop
-  grunt.registerTask('develop', ['setup', 'less', 'autoprefixer', 'concat', 'uglify', 'imagemin', 'connect', 'htmlinspector',
-    'watch']);
+	//Develop
+	grunt.registerTask('develop', ['setup', 'less', 'autoprefixer', 'concat', 'uglify', 'connect',
+	'watch']);
 
-  //Build
-  grunt.registerTask('build', ['combine', 'compress', 'imagemin', 'modernizer', 'uncss', 'usemin', 'zopfli']);
+	//Build
+	grunt.registerTask('build', ['combine', 'compress', 'imagemin', 'modernizer', 'uncss', 'usemin', 'zopfli']);
 
-  //Default task
-  grunt.registerTask('default', ['develop']);
+	//Default task
+	grunt.registerTask('default', ['develop']);
 };
